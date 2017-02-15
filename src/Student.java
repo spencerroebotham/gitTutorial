a82284f7ca7f70f8e6106170f9e86ce8b12fb7a9
spencerroebotham <spencer.roebotham@hotmail.com>
2017-02-15 13:44:17 -0500

    Added class Course, made changes to Student and Instructor

--- src/Student.java
+++ src/Student.java
@@ -3,6 +3,7 @@
  */
 public class Student extends Person {
     String major;
+    List<Course> courseList;
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
@@ -11,5 +12,9 @@ public class Student extends Person {
 
     public String toString() {
         return String.format("Name: %s\nMajor: %s\nEmail: %s", this.name, this.major, this.emailAddress);
+    }
+
+    public void addCourse(Course c) {
+
     }
 }
